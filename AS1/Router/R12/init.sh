caf808996338f3a550354449f2cb503f7d2dc732
Slaeesh <mathis.pateau@orange.fr>
2025-12-10 21:06:53 +0100

    init des trucs restants

--- AS1/Router/R12/init.sh
+++ AS1/Router/R12/init.sh
@@ -3,21 +3,24 @@
 # 1. Activation du routage
 sysctl -w net.ipv4.ip_forward=1
 
-echo "--- Config R12 ---"
-
-# --- eth0 : Vers R11 ---
+# --- eth2 : Vers R11 ---
 # Réseau 120.0.38.0/24. R11 est .1, donc R12 est .2
-ip addr flush dev eth0
-ip link set dev eth0 down
-ip addr add 120.0.38.2/24 dev eth0
-ip link set dev eth0 up
+ip addr flush dev eth2
+ip link set dev eth2 up
+ip addr add 120.0.38.2/24 dev eth2
 
 # --- eth1 : Vers Serveur DNS ---
 # Réseau 120.0.36.0/24. R12 est la passerelle .1
 ip addr flush dev eth1
-ip link set dev eth1 down
-ip addr add 120.0.36.1/24 dev eth1
 ip link set dev eth1 up
+ip addr add 120.0.36.1/24 dev eth1
+
+# --- eth0 : Vers R_FAI_1
+ip addr flush dev eth0
+ip link set dev eth0 up
+ip addr add 120.0.39.2/24 dev eth0
+
+
 
 # Lancement de FRRouting
 /usr/lib/frr/docker-start
--- AS1/Router/R_FAI_1/init.sh
+++ AS1/Router/R_FAI_1/init.sh
@@ -3,41 +3,31 @@
 # 1. Activation du forwarding (Base du routeur)
 sysctl -w net.ipv4.ip_forward=1
 
-echo "--- Configuration des IPs pour R_FAI_1 ---"
 
-# --- eth0 : Vers Box B1 (Clients Particuliers 1) ---
-ip addr flush dev eth0
-ip link set dev eth0 down
-ip addr add 120.0.32.1/24 dev eth0
-ip link set dev eth0 up
-
-# --- eth1 : Vers Box C1 (Clients Particuliers 2) ---
-ip addr flush dev eth1
-ip link set dev eth1 down
-ip addr add 120.0.33.1/24 dev eth1
-ip link set dev eth1 up
-
-# --- eth2 : Vers R_Entreprise1 ---
-ip addr flush dev eth2
-ip link set dev eth2 down
-ip addr add 120.0.34.1/24 dev eth2
-ip link set dev eth2 up
-
-# --- eth3 : Vers R11 (Services DNS/Web) ---
-ip addr flush dev eth3
-ip link set dev eth3 down
-ip addr add 120.0.35.1/24 dev eth3
-ip link set dev eth3 up
-
-# --- eth4 : Vers R_bordure_1 (Sortie vers AS2) ---
-# On utilise le subnet 120.0.39.0/24
+# --- eth4 : Vers Box B1 (Clients Particuliers 1) ---
 ip addr flush dev eth4
-ip link set dev eth4 down
-ip addr add 120.0.39.1/24 dev eth4
 ip link set dev eth4 up
+ip addr add 120.0.32.1/24 dev eth4
 
-# Vérification
-ip addr show
+# --- eth3 : Vers Box C1 (Clients Particuliers 2) ---
+ip addr flush dev eth3
+ip link set dev eth3 up
+ip addr add 120.0.33.1/24 dev eth3
+
+# --- eth0 : Vers R_Entreprise1 ---
+ip addr flush dev eth0
+ip link set dev eth0 up
+ip addr add 120.0.34.1/24 dev eth0
+
+# --- eth1 : Vers R11 (Services DNS/Web) ---
+ip addr flush dev eth1
+ip link set dev eth1 up
+ip addr add 120.0.35.1/24 dev eth1
+
+# --- eth2 : Vers R12 (---
+ip addr flush dev eth2
+ip link set dev eth2 up
+ip addr add 120.0.39.1/24 dev eth2
 
 echo "--- Lancement de FRRouting ---"
 /usr/lib/frr/docker-start
