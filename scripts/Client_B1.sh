3a4fdde1cc7a4aea8395bf5919ffb206d3ba43a1
gboss226 <cherifganema@gmail.com>
2025-12-12 14:31:56 +0100

    Update Client_B1.sh

--- scripts/Client_B1.sh
+++ scripts/Client_B1.sh
@@ -6,4 +6,4 @@ docker exec --privileged Client_B1 ip link set up dev eth0
 
 # Default Gateway -> Box_B1 (192.168.101.1)
 docker exec --privileged Client_B1 ip route del default || true
-docker exec --privileged Client_B1 ip route add default via 192.168.101.1
+docker exec --privileged Client_B1 ip route add default via 192.168.101.5
