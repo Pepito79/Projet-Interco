d1ba871e80b313d6c73be58ee0b23c5bf2e9a14e
Slaeesh <mathis.pateau@orange.fr>
2025-12-12 15:04:11 +0100

    DHCP fixed avec DNS qui marche bien

--- scripts/Client_C1.sh
+++ scripts/Client_C1.sh
@@ -7,3 +7,6 @@ docker exec --privileged Client_C1 ip link set up dev eth0
 # Default Gateway -> Box_C1 (192.168.2.1)
 docker exec --privileged Client_C1 ip route del default || true
 docker exec --privileged Client_C1 ip route add default via 192.168.2.5
+
+# Force DNS to internal server
+docker exec --privileged Client_C1 sh -c "echo 'nameserver 120.0.36.2' > /etc/resolv.conf"
